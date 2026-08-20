69ebaad83926fbeab5809fb81601f49159b60f94
STGknight <114973477+STGknight@users.noreply.github.com>
2026-08-20 09:19:07 -0700

    Add inventory item functionality
    Added user input for item name, quantity, and price. Items are now stored in the inventory dictionary using nested key-value data.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+inventory = {}
+
 print ("===Inventory Mangement System===")
 
 print("1. View Inventory")
 print("2. Add Item")
-print("3. Print")
+print("3. Exit")
 
 choice = input("Chose an option :")
 
 if choice == "1":
-    print("Viewing Inventory")
+    print(inventory)
 elif choice == "2":
-    print("Adding Item")
+    item_name = input("Enter item name: ")
+    quantity = input("Enter quantity: ")
+    price = input("Enter price: ")
+    
+    inventory[item_name]={
+        "quantity": quantity
+        "price": price
+    }
+
+    print("Item added.")
+    print(inventory)
+
 elif choice == "3":
     print("Goodbye!")
 else:
