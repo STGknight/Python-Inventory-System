338019d06e6b6f5464cef143b69a09640389eeb8
STGknight <114973477+STGknight@users.noreply.github.com>
2026-08-25 13:44:02 -0700

    Add inventory loop and item data handling
    - Added a persistent menu loop using while True
    - Added break logic for exiting the program
    - Improved inventory display with a dictionary loop
    - Converted quantity to int and price to float
    - Kept item data stored in nested dictionaries

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 inventory = {}
 
-print ("===Inventory Mangement System===")
+while True:
+    print ("===Inventory Mangement System===")
 
-print("1. View Inventory")
-print("2. Add Item")
-print("3. Exit")
+    print("1. View Inventory")
+    print("2. Add Item")
+    print("3. Exit")
 
-choice = input("Chose an option :")
+    choice = input("Chose an option :")
 
-if choice == "1":
-    print(inventory)
-elif choice == "2":
-    item_name = input("Enter item name: ")
-    quantity = input("Enter quantity: ")
-    price = input("Enter price: ")
+    if choice == "1":
+        for item_name, item_info in inventory.items():
+            print ("Item:", item_name)
+            print("Quantity:", item_info["quantity"])
+            print("Price: $", item_info["price"])
+    elif choice == "2":
+        item_name = input("Enter item name: ")
+        quantity = input("Enter quantity: ")
+        price = input("Enter price: ")
     
-    inventory[item_name]={
-        "quantity": quantity
-        "price": price
-    }
+        inventory[item_name]={
+            "quantity": quantity,
+            "price": price
+        }
 
-    print("Item added.")
-    print(inventory)
+        print("Item added.")
+        print(inventory)
 
-elif choice == "3":
-    print("Goodbye!")
-else:
-    print("Invail option")
+    elif choice == "3":
+        print("Goodbye!")
+        break
+    else:
+        print("Invail option")
