6efbc5f4cabfb3fbf5a88bf4cb5bed6b97b1b69d
STGknight <114973477+STGknight@users.noreply.github.com>
2026-08-27 10:11:39 -0700

    Add remove and update inventory features
    - Added remove item functionality
    - Added update item functionality
    - Added quantity and price type conversion
    - Added item existence checks

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ while True:
 
     print("1. View Inventory")
     print("2. Add Item")
-    print("3. Exit")
+    print("3. Remove Item")
+    print("4. Update Item")
+    print("5. Exit")
 
     choice = input("Chose an option :")
 
@@ -16,8 +18,8 @@ while True:
             print("Price: $", item_info["price"])
     elif choice == "2":
         item_name = input("Enter item name: ")
-        quantity = input("Enter quantity: ")
-        price = input("Enter price: ")
+        quantity = int(input("Enter quantity: "))
+        price = float(input("Enter price: "))
     
         inventory[item_name]={
             "quantity": quantity,
@@ -27,7 +29,34 @@ while True:
         print("Item added.")
         print(inventory)
 
-    elif choice == "3":
+    elif choice =="3":
+        item_name = input("Enter item name to remove: ")
+
+        if item_name in inventory:
+            del inventory[item_name]
+            print("item removed.")
+        else:
+            print("Item not found.")
+    
+    elif choice == "4":
+        item_name = input("Enter item name to update: ")
+
+        if item_name in inventory:
+            print("Item found.")
+
+            new_quantity = int(input("Enter new quantity: "))
+            new_price = float(input("Enter new Price: "))
+
+            inventory[item_name]["quantity"] = new_quantity 
+            inventory[item_name]["price"] = new_price
+
+            print("Item updated."
+            )
+        else:
+            print("Item not found.")
+        
+
+    elif choice == "5":
         print("Goodbye!")
         break
     else:
